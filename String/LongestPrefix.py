5f5ddb3f5ccb48c3a9abeea5f163eb8b35c71418
Amisha Naik <132745732+amishana1k@users.noreply.github.com>
2024-02-06 00:42:18 +0530

    LongestPrefix.py

--- String/LongestPrefix.py
+++ String/LongestPrefix.py
@@ -1,15 +1,18 @@
-strs = ["flower","flow","flight"]
 
-res = ""
-i = 0
-while i < len(strs)-1:
-    s = strs[i]
-    r = strs[i + 1]
-    for j in range(0,len(s)):
-        if s[j] != r[j]:
-            continue
-        res += s[j]
-        j += 1
-    i += 1
-print(res)
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        strs.sort()
+
+        s = strs[0]
+        for i, char in enumerate(s):
+            for other_string in strs[1:]:
+                if i >= len(other_string) or other_string[i] != char:
+                    return s[:i]
+        return s
+
+solution = Solution()
+strs = ["flower", "flow", "flight"]
+print(solution.longestCommonPrefix(strs))
 
